544ad21944701056e9602d512e43a7156b905172
huaxk <huaxk@163.com>
2020-01-10 15:17:19 +0800

    create table

--- tests/connection.nim
+++ tests/connection.nim
@@ -3,5 +3,13 @@ import os, db_postgres
 let password = getEnv("password")
 
 let db = open("localhost", "test", "test", "test")
-let tables = db.getAllRows(sql"select tablename from pg_tables")
+let createsql = sql"""
+  create table if not exsits cities(
+    id serial primary key,
+    name varchar(50) not null,
+    lnglat geometry
+  );
+"""
+db.exec(createsql)
+let tables = db.getAllRows(sql"select tablename from pg_tables where schemaname='public'")
 echo tables
