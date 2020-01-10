c2ac7084c2e51aad276b5c530d4ed8939b3b10ff
huaxk <huaxk@163.com>
2020-01-10 14:57:08 +0800

    connect to pg

--- tests/connection.nim
+++ tests/connection.nim
@@ -2,6 +2,6 @@ import os, db_postgres
 
 let password = getEnv("password")
 
-let db = open("localhost", "test", password, "test")
+let db = open("localhost", "test", "test", "test")
 let tables = db.getAllRows(sql"select tablename from pg_tables")
 echo tables
