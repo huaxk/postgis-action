a58bd3cfb074a33eb6a6523b8bfa0a80ca77ce27
huaxk <huaxk@163.com>
2020-01-10 16:47:58 +0800

    test insert and query

--- tests/connection.nim
+++ tests/connection.nim
@@ -1,15 +1,40 @@
-import os, db_postgres
+import unittest, os, db_postgres, sequtils, strformat
+
+# let params = commandLineParams()
+# let password = if params.len == 0: "test" else: "test"
+# echo params
+# echo password
 
 let password = getEnv("password")
+echo password
+echo password.len
 
-let db = open("localhost", "test", "test", "test")
-let createsql = sql"""
-  create table if not exsits cities(
-    id serial primary key,
-    name varchar(50) not null,
-    lnglat geometry
-  );
-"""
-db.exec(createsql)
-let tables = db.getAllRows(sql"select tablename from pg_tables where schemaname='public'")
-echo tables
+suite "Test Postgis":
+  echo "connect to pg"
+
+  let db = open("localhost", "test", password, "test")
+
+  test "check spatial_ref_sys table":
+    let tables = db.getAllRows(sql"select tablename from pg_tables where schemaname='public'").mapIt(it[0])
+    check "spatial_ref_sys" in tables
+
+  test "create table with geometry":
+    let createsql = sql"""
+      create table if not exists cities(
+        id serial primary key,
+        name varchar(50) not null,
+        lnglat geometry
+      );
+    """
+    db.exec(createsql)
+    let tables = db.getAllRows(sql"select tablename from pg_tables where schemaname='public'").mapIt(it[0])
+    check "cities" in tables
+
+  test "insert and query geometry data":
+    let
+      name = "huaian"
+      wkt = "POINT(1 2)"
+    let insertsql = fmt"insert into cities (name, lnglat) values ('{name}', st_geomfromtext('{wkt}'))"
+    db.exec(sql(insertsql))
+    let row = db.getRow(sql"select name, st_astext(lnglat) from cities")
+    check row == [name, wkt]
