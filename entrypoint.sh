5491670997755c9d1759de9f7a60266a8d4ec63f
kevin <kgrenier@wishibam.com>
2020-12-07 12:02:39 +0100

    Allow to expore another port than 5432 default one

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 docker_run="docker run"
 docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
 docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
-docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
-docker_run="$docker_run -d -p 5432:5432 mdillon/postgis:$INPUT_POSTGRESQL_VERSION"
+docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_POSTGRES_PASSWORD"
+docker_run="$docker_run -d -p $INPUT_POSTGRESQL_PORT:5432 mdillon/postgis:$INPUT_POSTGRESQL_VERSION"
 
 sh -c "$docker_run"
